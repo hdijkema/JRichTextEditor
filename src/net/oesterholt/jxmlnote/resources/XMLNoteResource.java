d7fd74105183595f6df08bda084fd22260beb535
Hans Oesterholt <hans@oesterholt.net>
2013-04-28 09:45:59 +0200

    Adjusted the license information

--- src/net/oesterholt/jxmlnote/resources/XMLNoteResource.java
+++ src/net/oesterholt/jxmlnote/resources/XMLNoteResource.java
@@ -1,21 +1,19 @@
 /* ******************************************************************************
  *
  *       Copyright 2008-2010 Hans Oesterholt-Dijkema
- *       This file is part of the JDesktop SwingX library
- *       and part of the SwingLabs project
  *
- *   SwingX is free software: you can redistribute it and/or modify
+ *   JRichTextEditor is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU Lesser General Public License as 
  *   published by the Free Software Foundation, either version 3 of 
  *   the License, or (at your option) any later version.
  *
- *   SwingX is distributed in the hope that it will be useful,
+ *   JRichTextEditor is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *   GNU Lesser General Public License for more details.
  *
  *   You should have received a copy of the GNU Lesser General Public License
- *   along with SwingX.  If not, see <http://www.gnu.org/licenses/>.
+ *   along with JRichTextEditor.  If not, see <http://www.gnu.org/licenses/>.
  *   
  * ******************************************************************************/
 
@@ -34,13 +32,13 @@ public class XMLNoteResource {
 	
 	public static InputStream get(String resourceName) throws IOException {
 		try {
-			FileInputStream fin=new FileInputStream("org.jdesktop.swingx.jxmlnote/resouces/"+resourceName);
+			FileInputStream fin=new FileInputStream("org.jdesktop.JRichTextEditor.jxmlnote/resouces/"+resourceName);
 			return fin;
 		} catch (FileNotFoundException e) {
 			if (_resources==null) {
 				_resources=new JarFile("JXMLNote.jar");
 			}
-			ZipEntry entry=_resources.getEntry("org.jdesktop.swingx.jxmlnote/resources"+resourceName);
+			ZipEntry entry=_resources.getEntry("org.jdesktop.JRichTextEditor.jxmlnote/resources"+resourceName);
 			return _resources.getInputStream(entry);
 		}
 	}
