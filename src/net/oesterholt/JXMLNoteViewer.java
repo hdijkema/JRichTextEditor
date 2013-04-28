7e6c319b4d5aa8c23142ff632b18a4e52bd0f339
Hans Oesterholt-Dijkema <hans@oesterholt.net>
2013-04-28 10:06:45 +0300

    Update JXMLNoteViewer.java

--- src/net/oesterholt/JXMLNoteViewer.java
+++ src/net/oesterholt/JXMLNoteViewer.java
@@ -1,10 +1,8 @@
 /* ******************************************************************************
  *
  *       Copyright 2008-2010 Hans Oesterholt-Dijkema
- *       This file is part of the JDesktop SwingX library
- *       and part of the SwingLabs project
  *
- *   SwingX is free software: you can redistribute it and/or modify
+ *   JRichTextEditor is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU Lesser General Public License as 
  *   published by the Free Software Foundation, either version 3 of 
  *   the License, or (at your option) any later version.
@@ -15,7 +13,7 @@
  *   GNU Lesser General Public License for more details.
  *
  *   You should have received a copy of the GNU Lesser General Public License
- *   along with SwingX.  If not, see <http://www.gnu.org/licenses/>.
+ *   along with JRichTextEditor.  If not, see <http://www.gnu.org/licenses/>.
  *   
  * ******************************************************************************/
 
